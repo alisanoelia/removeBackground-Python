19628007d5db31541d2afa2c159d47c8bfb0c1db
alyssa <alyssa>
2023-09-19 01:01:24 +0000

    firts commit

---
+++ app.py
@@ -0,0 +1,15 @@
+from rembg import remove
+from PIL import Image
+import sys
+
+def main():
+    input_path = sys.argv[1]
+    output_path = sys.argv[2]
+
+    input = Image.open(input_path)
+
+    output = remove(input)
+    output.save(output_path)
+
+if __name__ == "__main__":
+    main()
